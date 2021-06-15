468ebf9d8a19704b012abdeaf4b45fa3f76f434e
zhanghn <zhang.h.n@foxmail.com>
2021-06-15 01:25:49 +0000

    remove some unnessary info

--- src/main/java/org/gcszhn/system/service/dao/impl/UserDaoServiceImpl.java
+++ src/main/java/org/gcszhn/system/service/dao/impl/UserDaoServiceImpl.java
@@ -83,7 +83,6 @@ public class UserDaoServiceImpl implements UserDaoService {
             throw new UDException(e);
         }
     }
-    @SuppressWarnings("unchecked")
     @Override
     public int verifyUser(User user) {
         try {
